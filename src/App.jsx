b0133f43e39324e593257a50e03c980a6d11811e
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-03 22:30:56 +0200

    creation pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";  // Homepage component
 import LoginPage from "./pages/LoginPage";  // Login page component
 import DashboardPage from "./pages/DashboardPage";  // Dashboard page component
-//import AdminPanel from "./pages/AdminPanel";  // Admin panel page component
-// import Categories from "./pages/Categories";  // Categories page component
-//import Transactions from "./pages/Transactions";  // Transactions page component
-//import Analytics from "./pages/Analytics";  // Analytics page component
-// import Logout from "./pages/Logout";  // Logout page component
+import AdminPanel from "./pages/AdminPanel";
+import ProfitGoal from "./pages/ProfitGoal"
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Homepage */}
-        <Route path="/login" element={<LoginPage />} />  {/* Login Page */}
-        <Route path="/dashboard" element={<DashboardPage />} />  {/* Dashboard */}
-        {/* <Route path="/admin" element={<AdminPanel />} />  Admin Panel */}
-        {/* <Route path="/categories" element={<Categories />} />  Categories */}
-        {/* <Route path="/transactions" element={<Transactions />} />  Transactions */}
-        {/* <Route path="/analytics" element={<Analytics />} />  Analytics */}
-        {/* <Route path="/logout" element={<Logout />} />  Logout */}
+        <Route path="/" element={<Home />} />  
+        <Route path="/login" element={<LoginPage />} />  
+        <Route path="/dashboard" element={<DashboardPage />} />  
+        <Route path="/admin" element={<AdminPanel />} />  
+        <Route path="/profit" element={<ProfitGoal />} /> 
       </Routes>
     </Router>
   );
