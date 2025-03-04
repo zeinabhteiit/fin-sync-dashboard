5c3754b998665d2ef157c392e18b1b9c1d179492
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-04 09:04:42 +0200

    sidebar incorporation of new pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
         <li><Link to="/login">Login</Link></li>  {/* Link to login page */}
         <li><Link to="/dashboard">Dashboard</Link></li>  {/* Link to dashboard page */}
         <li><Link to="/admin">Admin Panel</Link></li>  {/* Link to admin panel */}
-        {/* <li><Link to="/categories">Categories</Link></li>  Link to categories page */}
+        <li><Link to="/profit">Profit Goals</Link></li>{/* Link to Profit Goal Page */}
         <li><Link to="/transactions">Transactions</Link></li>  {/* Link to transactions page */}
         <li><Link to="/analytics">Analytics</Link></li>  {/* Link to analytics page */}
         <li><Link to="/logout">Logout</Link></li>  {/* Link to logout page (You can add this functionality) */}
